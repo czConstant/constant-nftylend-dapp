db7b50eecb785a424a9f22533fbdc71331a52b5f
Yammoe <loc@myconstant.com>
2022-03-09 09:35:16 +0700

    Update url to use api base

--- src/common/constants/url.ts
+++ src/common/constants/url.ts
@@ -1,6 +1,7 @@
 export const APP_ENV = DOT_ENV;
-export const API_BASE = APP_ENV.REACT_MCT_ROOT;
+export const MCT_ROOT = APP_ENV.REACT_MCT_ROOT;
 export const BASE_PATH = APP_ENV.REACT_BASE_PATH;
+export const MCT_API_BASE = APP_ENV.REACT_MCT_API_BASE;
 
 export const APP_URL = {
   HOME: `${BASE_PATH}/`,
@@ -12,13 +13,13 @@ export const APP_URL = {
 
 export const API_URL = {
   NFT_LEND: {
-    COLLECTIONS: `${API_BASE}/nfty-lend-api/collections/list`,
-    COLLECTION_BY_ID: `${API_BASE}/nfty-lend-api/collections/detail`,
-    ALL_LISTING_LOANS: `${API_BASE}/nfty-lend-api/loans/listing`,
-    GET_LOANS: `${API_BASE}/nfty-lend-api/loans/list`,
-    GET_OFERS: `${API_BASE}/nfty-lend-api/loans/offers`,
-    LOANS_BY_ID: `${API_BASE}/nfty-lend-api/assets/detail`,
-    LIST_CURRENCY: `${API_BASE}/nfty-lend-api/currencies/list`,
-    UPDATE_BLOCK: `${API_BASE}/nfty-lend-api/blockchain/update-block`,
+    COLLECTIONS: `${MCT_ROOT}${MCT_API_BASE}/collections/list`,
+    COLLECTION_BY_ID: `${MCT_ROOT}${MCT_API_BASE}/collections/detail`,
+    ALL_LISTING_LOANS: `${MCT_ROOT}${MCT_API_BASE}/loans/listing`,
+    GET_LOANS: `${MCT_ROOT}${MCT_API_BASE}/loans/list`,
+    GET_OFERS: `${MCT_ROOT}${MCT_API_BASE}/loans/offers`,
+    LOANS_BY_ID: `${MCT_ROOT}${MCT_API_BASE}/assets/detail`,
+    LIST_CURRENCY: `${MCT_ROOT}${MCT_API_BASE}/currencies/list`,
+    UPDATE_BLOCK: `${MCT_ROOT}${MCT_API_BASE}/blockchain/update-block`,
   }
 }
