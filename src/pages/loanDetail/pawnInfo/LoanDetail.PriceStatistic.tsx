fa314fae30bae9b59cfb76445b4446b4278765b7
Yammoe <loc@myconstant.com>
2022-05-23 15:58:47 +0700

    Fix loan to value calculation

--- src/pages/loanDetail/pawnInfo/LoanDetail.PriceStatistic.tsx
+++ src/pages/loanDetail/pawnInfo/LoanDetail.PriceStatistic.tsx
@@ -13,7 +13,7 @@ interface LoanDetailPriceStatisticProps {
 
 const LoanDetailPriceStatistic: React.FC<LoanDetailPriceStatisticProps> = ({ loan }) => {
   const ltv = loan.asset?.stats?.avg_price 
-    ? new BigNumber(loan.principal_amount).dividedBy(loan.asset?.stats?.avg_price).toNumber()
+    ? new BigNumber(loan.principal_amount).dividedBy(loan.asset?.stats?.avg_price).multipliedBy(loan.asset?.stats?.currency?.price).toNumber()
     : 0;
 
   return (
