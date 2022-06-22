58fb37290723c86dc3fe7cfef259a1dcd84a984e
Yammoe <loc@myconstant.com>
2022-06-22 14:01:47 +0700

    Update format repay amount

--- src/pages/loanDetail/pawnInfo/LoanDetail.InEscrow.tsx
+++ src/pages/loanDetail/pawnInfo/LoanDetail.InEscrow.tsx
@@ -167,7 +167,7 @@ const LoanDetailInEscrow: React.FC<LoanDetailInEscrowProps> = ({ loan }) => {
       <div className={styles.info}>
         <div>
         <Text color='text.secondary' fontWeight='medium' fontSize='xs'>Repayment Amount</Text>
-          <div className={styles.value}>{formatCurrency(Number(payAmount), 8)} {loan.currency?.symbol}</div>
+          <div className={styles.value}>{formatCurrency(payAmount)} {loan.currency?.symbol}</div>
         </div>
         <div>
           <Text color='text.secondary' fontWeight='medium' fontSize='xs'>APR</Text>
