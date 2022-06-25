f6281fc94967edbbf4164395d4c5b9f6bb7a2675
Yammoe <loc@myconstant.com>
2022-06-25 19:23:56 +0700

    Refactor wallet content provider

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ import MyLoadingOverlay from 'src/common/components/myLoadingOverlay';
 
 import AppRouter from './navigation';
 import { getSystemConfigs } from './modules/nftLend/api';
-import { SolanaWalletProvider } from './modules/solana/hooks/withWalletProvider';
+// import { SolanaWalletProvider } from './modules/solana/hooks/withWalletProvider';
 import { NearWalletProvider } from './modules/near/hooks/withNearWalletProvider';
 
 import { useAppDispatch } from './store/hooks';
 import { updateConfigs } from './store/nftyLend';
 import { useDetectConnectedWallet } from './modules/nftLend/hooks/useDetectWallet';
 import customTheme from './theme';
+import { MyWalletProvider } from './modules/nftLend/context/myWalletContext';
 
 export default function App() {
   const dispatch = useAppDispatch();
@@ -32,15 +33,16 @@ export default function App() {
     <ChakraProvider theme={customTheme}>
       <BrowserRouter>
         <NearWalletProvider>
-          <SolanaWalletProvider>
-          {/* <Scrollbars  style={{ width: `100vw`, height: `100vh` }}> */}
-            <AppRouter />
-          {/* </Scrollbars> */}
-          <ModalManager />
-          <ToastContainer />
-          <MyLoadingOverlay />
-
-          </SolanaWalletProvider>
+          <MyWalletProvider>
+          {/* <SolanaWalletProvider> */}
+            {/* <Scrollbars  style={{ width: `100vw`, height: `100vh` }}> */}
+              <AppRouter />
+            {/* </Scrollbars> */}
+            <ModalManager />
+            <ToastContainer />
+            <MyLoadingOverlay />
+          </MyWalletProvider>
+          {/* </SolanaWalletProvider> */}
         </NearWalletProvider>
       </BrowserRouter>
     </ChakraProvider>
