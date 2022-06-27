d8600eab4de301f8b2775aa594ccee03893bf298
Yammoe <loc@myconstant.com>
2022-06-27 09:54:21 +0700

    Add filter timeframe chart

--- src/views/dashboard/affiliates/volumeChart.tsx
+++ src/views/dashboard/affiliates/volumeChart.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@chakra-ui/react'
-import moment from 'moment-timezone'
 import { useEffect, useMemo, useState } from 'react'
+import moment from 'moment-timezone'
 import ReactApexChart from 'react-apexcharts'
+import { Box, Button, Flex } from '@chakra-ui/react'
 
 import { getAffiliateVolumes } from 'src/modules/nftLend/api'
 import { useCurrentWallet } from 'src/modules/nftLend/hooks/useCurrentWallet'
@@ -10,17 +10,18 @@ const VolumeChart = () => {
   const { currentWallet } = useCurrentWallet()
 
   const [volume, setVolume] = useState<Array<any>>([])
+  const [filter, setFilter] = useState<string>('week')
 
   useEffect(() => {
     getAffiliateVolumes({
       address: currentWallet.address,
       network: currentWallet.chain,
       limit: 12,
-      rpt_by: 'week',
+      rpt_by: filter,
     }).then(res => {
       setVolume(res.result.reverse())
     })
-  }, [currentWallet])
+  }, [currentWallet, filter])
 
   const series = useMemo(() => [{
     name: 'NEAR Volume',
@@ -46,7 +47,7 @@ const VolumeChart = () => {
     },
     stroke: {
       show: true,
-      width: 2,
+      width: 12,
       colors: ['transparent']
     },
     xaxis: {
@@ -80,6 +81,10 @@ const VolumeChart = () => {
 
   return (
     <Box id='chart'>
+      <Flex gap={4} justifyContent='flex-end'>
+        <Button fontSize='sm' opacity={filter === 'week' ? 1 : 0.5} variant='link' color='text.primary' onClick={() => setFilter('week')}>WEEK</Button>
+        <Button fontSize='sm' opacity={filter === 'month' ? 1 : 0.5} variant='link' color='text.primary' onClick={() => setFilter('month')}>MONTH</Button>
+      </Flex>
       <ReactApexChart options={options} series={series} type='bar' height={400} />
     </Box>
   )
