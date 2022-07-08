b8b586224d8c6ffb4085db35cf653c4ec6bdb2b5
Yammoe <loc@myconstant.com>
2022-07-08 14:43:50 +0700

    No require login

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -1,19 +1,12 @@
-import { Box, Button, Flex, Heading, Icon, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import moment from 'moment-timezone';
-import { useLocation, Navigate } from 'react-router-dom';
 
 import BodyContainer from 'src/common/components/bodyContainer';
-import { APP_URL } from 'src/common/constants/url';
-import { useCurrentWallet } from 'src/modules/nftLend/hooks/useCurrentWallet';
 import Countdown from 'src/views/leaderboard/countdown';
 import Rules from 'src/views/leaderboard/rules';
 import TopBorrower from 'src/views/leaderboard/topBorrower';
 
 const Leaderboard = () => {
-  const { isConnected } = useCurrentWallet()
-
-  if (!isConnected) return <Navigate to={APP_URL.HOME} />
-
   return (
     <BodyContainer>
       <Flex alignItems='center' justifyContent='space-between' m={8}>
